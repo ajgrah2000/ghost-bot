b3d338f7a403dbf16fffecd81ef4c7237bad0294
me <a@localhost>
2025-01-11 10:29:47 +1100

    Removing the need for the 'empty.blend' file

--- scripts/blender-render.py
+++ scripts/blender-render.py
@@ -1,8 +1,10 @@
 # Import a single 'wrl' and render it (without any fancy stuff).
+# 'Tip' to see how blender settings translate to python, enable the 'scripting' window before clicking on buttons.
 import bpy
 import argparse
 import sys
 import math
+import functools
 
 def get_args():
     """ Arguments to the script need to be after '--'.
@@ -26,7 +28,38 @@ def get_args():
 
     return args
 
-def import_and_render(input_file, output_file, object_rotation, camera_location, scale_factor):
+def set_renderer_options():
+    bpy.context.scene.render.engine = 'CYCLES'
+    # Set an upper time limit of rendering 
+    bpy.context.scene.cycles.time_limit = 180 # seconds
+
+    # If there's no 'denoiser', then disable denoising.
+    if bpy.context.scene.cycles.denoiser == '':
+        print("Disabling denoiser")
+        # Need to disable denoising for some builds of blender.
+        bpy.context.scene.cycles.use_denoising = False
+        bpy.context.view_layer.cycles.use_denoising = False
+
+def import_and_render(input_file, output_file, object_rotation, camera_location):
+
+    # Create an empty scene
+    bpy.ops.scene.new(type='EMPTY')
+
+    # Create new camera and set as active:
+    camera = bpy.data.cameras.new('Camera')
+    camera_object = bpy.data.objects.new('Camera', camera)
+    bpy.context.collection.objects.link(camera_object)
+    bpy.context.scene.camera = camera_object
+    bpy.context.scene.camera.data.lens_unit = 'FOV'
+    bpy.context.scene.camera.data.angle = math.radians(60)
+
+    # Create a light source:
+    for (i, location) in enumerate([(-2.0, -3.0, 2.0), (3.0, -3.0, 2.0)]):
+        light = bpy.data.lights.new(f'Light{i}', type='POINT')
+        light.energy = 200
+        light_object = bpy.data.objects.new(f'Light{i}', object_data=light)
+        bpy.context.collection.objects.link(light_object)
+        light_object.location = location
 
     assembly = bpy.data.objects.new('ImportedAssembly', None)
     bpy.context.collection.objects.link(assembly)
@@ -43,10 +76,11 @@ def import_and_render(input_file, output_file, object_rotation, camera_location,
     target = bpy.data.objects.get('ImportedAssembly', False)
 
     # Constraint the camera to 'look' at the target.
-    constraint = bpy.data.objects['Camera'].constraints.new('TRACK_TO')
-    constraint.target = target
-    constraint.track_axis = 'TRACK_NEGATIVE_Z'
-    constraint.up_axis =  'UP_Y'
+
+    constraint2 = bpy.context.scene.camera.constraints.new('TRACK_TO')
+    constraint2.target = target
+    constraint2.track_axis = 'TRACK_NEGATIVE_Z'
+    constraint2.up_axis =  'UP_Y'
 
     # Moves object origin to 3D cursor wherever the cursor is located.
     bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
@@ -54,18 +88,21 @@ def import_and_render(input_file, output_file, object_rotation, camera_location,
 
     target.select_set(True)
 
-    # Get the largest dimension from the selected objects (poor person's bounding box).
-    max_x = max([b.dimensions.x for b in bpy.context.selected_objects])
-    max_y = max([b.dimensions.y for b in bpy.context.selected_objects])
-    max_z = max([b.dimensions.z for b in bpy.context.selected_objects])
+    # Get the diagonal of the rectangular bounding box (to figure out overall size).
+    min_x = min([b.location.x for b in bpy.context.selected_objects])
+    min_y = min([b.location.y for b in bpy.context.selected_objects])
+    min_z = min([b.location.z for b in bpy.context.selected_objects])
+    max_x = max([b.location.x for b in bpy.context.selected_objects])
+    max_y = max([b.location.y for b in bpy.context.selected_objects])
+    max_z = max([b.location.z for b in bpy.context.selected_objects])
 
-    scale = max(max_x, max_y, max_z)
+    v = (max_x - min_x, max_y - min_y, max_z - min_z)
+    scale = math.sqrt(sum([x*x for x in v]))
 
-    # Fudge factor, place the camera further away..
-    scale *= scale_factor
-    print(scale)
+
+    scale /= math.atan(bpy.context.scene.camera.data.angle)
+
     # Set camera location
-    print(tuple(map(lambda x : x * scale, camera_location)))
     bpy.context.scene.camera.location = tuple(map(lambda x : x * scale, camera_location))
 
     bpy.context.scene.render.filepath = output_file
@@ -73,8 +110,11 @@ def import_and_render(input_file, output_file, object_rotation, camera_location,
 
 def main():
     args = get_args()
-    camera_location = (0.1, -0.3, 1.0) # Unit-ish vector (will be scaled by the size of the imported model)
-    # 'scale_factor' is a distance fudge for the camera.
-    import_and_render(args.input_file, args.output_file, args.rotation * math.pi / 180.0, camera_location, scale_factor=3.5)
+    camera_location = (0.1, -0.5, 1.0) # Rough 'direction', will convert to unit vector and add a 'fudge scale', final location is related to assembly size.
+    mag = math.sqrt(sum([x*x for x in camera_location]))
+    camera_location = tuple([v/mag for v in camera_location])
+    
+    set_renderer_options()
+    import_and_render(args.input_file, args.output_file, args.rotation * math.pi / 180.0, camera_location)
 
 main()
--- scripts/generate_render.sh
+++ scripts/generate_render.sh
@@ -6,9 +6,8 @@ pushd "$ROOT_DIR"
 BASE_INPUT=ghost-bot
 
 SOURCE_DIR=$ROOT_DIR/pcb
-BASE_BLENDER_SCENE=$ROOT_DIR/mech/empty.blend
 
 WRL_FILE=$ROOT_DIR/output/wrl/$BASE_INPUT.wrl
 
-blender -b $BASE_BLENDER_SCENE --python ./scripts/blender-render.py -- $WRL_FILE
-blender -b $BASE_BLENDER_SCENE --python ./scripts/blender-render.py -- $WRL_FILE --output_file=$WRL_FILE.180.png --rotation=180.0
+blender --background --python ./scripts/blender-render.py -- $WRL_FILE
+blender --background --python ./scripts/blender-render.py -- $WRL_FILE --output_file=$WRL_FILE.180.png --rotation=180.0
